24728cee30686760dc345309d2ef136eccab20c0
Tedwinder <TedCallow76@gmail.com>
2016-02-20 17:39:28 -0800

    OOP news_view.php
    Updated to OOP language per Thursday class.  Next version will call the News class within a session, so the shell will resemble session-test2.php from Thursday's class

--- news_view.php
+++ news_view.php
@@ -1,46 +1,78 @@
-<?php
+<?php 
 //news_view.php
+//config
 
 require 'inc_0700/config_inc.php';
-$config->titleTag = 'News Aggregator';
-$config->banner = 'NEWS';
 
-if(isset($_GET['id']) && (int)$_GET['id'] > 0)
-{//good data processed
-    $id = $_GET['id'];
+//header
+get_header();
+
+//set id in order to call feed
+if(isset($_GET['id']) && (int)$_GET['id'] > 0){//get data
+$id = $_GET['id'];
 }
 else
 {//bad data, redirected
     header('Location:index.php');
 }
 
-$sql = "SELECT * FROM p3_feeds WHERE CategoryID = $id";
+//calling constructor for News class to obtain the feed
+$myNews = new News($id);
 
-get_header();
+
+//footer
+get_footer(); 
+
+//create class to get News Feed
+
+class News
+{
+	public $CategoryID;
+	public $FeedName;
+	public $FeedDescription;
+
+	//create constructor function
+
+	public function __construct($id)
+	{
+
+		$this->CategoryID = (int)$id;
+
+		//SQL Statement
+
+		$sql = "SELECT * FROM p3_feeds WHERE CategoryID = $id";
+
+		//IDB::conn() creates a shareable database connection viea a singleton class
+		$result = mysqli_query(IDB::conn(), $sql) or die(trigger_error(mysqli_error(IDB::conn()), E_USER_ERROR));
+
+		if(mysqli_num_rows($result) > 0)
+			{#there are records - present data
+			while($row = mysqli_fetch_assoc($result))
+				{//pull data from array
+	
+				$this->FeedName = $row['FeedName'];
+				$this->FeedDescription = $row['FeedDescription'];
+				
+				//echo data
+				echo '<p>';
+				echo '<b>' . $this->FeedName . '</b><br />';
+	   			echo $this->FeedDescription;
+	   			//echo '<a href="' . $row['FeedURL'] . '">';
+	   			echo '<p>';
+	   			
+				} //end while statement
+			
+			
+			} //end if statement
+
+		echo '<p><a href="n_index.php">BACK</a></p>';
+		@mysqli_free_result($result);	
+
+	} //end function
+
+} //end class
+
 ?>
 
-<h3 align="center">News Aggregator</h3>
 
-<?php
-$result = mysqli_query(IDB::conn(), $sql) or die(trigger_error(mysqli_error(IDB::conn()), E_USER_ERROR));
 
-if(mysqli_num_rows($result) > 0)
-{#there are records - present data
-	while($row = mysqli_fetch_assoc($result))
-	{# pull data from associative array
-	   echo '<p>';
-	   echo '<b>' . $row['FeedName'] . '</b><br />';
-	   echo $row['FeedDescription'];
-	   //echo '<a href="' . $row['FeedURL'] . '">';
-	   echo '</p>';
-	}
-}else{#no records
-	echo '<div align="center">Sorry, there are no records that match this query</div>';
-}
-    
-echo '<p><a href="news_list.php">BACK</a></p>';
-
-@mysqli_free_result($result);
-
-get_footer();
-?>
