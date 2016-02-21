b307fc9778ebbd1c8dfa060e7b72ea7f596181b5
Tedwinder <TedCallow76@gmail.com>
2016-02-20 21:27:13 -0800

    OOP version of n_index

--- n_index.php
+++ n_index.php
@@ -1,34 +1,68 @@
 <?php
 //n_index.php
+//config
 
 require 'inc_0700/config_inc.php';
 
 $config->titleTag = 'News';
 $config->banner = 'NEWS';
 
-$sql = "SELECT * FROM p3_categories";
+//header
 
 get_header();
-?>
-<h3 align="center">News Categories</h3>
-<?php
-$result = mysqli_query(IDB::conn(), $sql) or die(trigger_error(mysqli_error(IDB::conn()), E_USER_ERROR));
 
-if(mysqli_num_rows($result) > 0)
-{#there are records - present data
-	while($row = mysqli_fetch_assoc($result))
-	{# pull data from associative array
-	   echo '<p>';
-	   echo '<b><a href="news_view.php?id=' . $row['CategoryID'] . '">' . $row['CategoryName'] . '</a></b><br />';
-	   echo $row['CategoryDescription'];
-       echo '</p>';
-	}
-}else
-{#no records
-	echo '<div align="center">Sorry, there are no records that match this query</div>';
-}
 
-@mysqli_free_result($result);
+$myNewsList = new Newslist();
 
 get_footer();
+
+class NewsList
+{
+	public $CategoryID;
+	public $CategoryName;
+	public $CategoryDescription;
+
+	//create constructor function
+
+	public function __construct()//deleted $id variable
+	{
+
+		//$this->CategoryID = (int)$id;
+
+		//SQL Statement
+
+		$sql = "SELECT * FROM p3_categories";
+
+		//IDB::conn() creates a shareable database connection viea a singleton class
+		$result = mysqli_query(IDB::conn(), $sql) or die(trigger_error(mysqli_error(IDB::conn()), E_USER_ERROR));
+
+		if(mysqli_num_rows($result) > 0)
+			{#there are records - present data
+			while($row = mysqli_fetch_assoc($result))
+				{//pull data from array
+				
+				$this->CategoryID = $row['CategoryID'];
+				$this->CategoryName = $row['CategoryName'];
+				$this->CategoryDescription = $row['CategoryDescription'];
+				
+				//echo data
+				echo '<p>';
+	   			echo '<b><a href="news_view.php?id=' . $this->CategoryID . '">' . $this->CategoryName . '</a></b><br />';
+	   			echo $this->CategoryDescription;
+	   			//echo '<a href="' . $row['FeedURL'] . '">';
+	   			echo '<p>';
+	   			
+				} //end while statement
+			
+			
+			} //end if statement
+
+
+		@mysqli_free_result($result);	
+
+	} //end function
+
+} //end class
+
+
 ?>
