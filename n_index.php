4c929a2ab01498718007f19ab4fc887b19ef1a1e
Tedwinder <TedCallow76@gmail.com>
2016-02-21 21:02:02 -0800

    changed to add date_diff - cache code not yet working

--- n_index.php
+++ n_index.php
@@ -1,6 +1,9 @@
 <?php
+
 //n_index.php
 //config
+
+session_start();
 
 require 'inc_0700/config_inc.php';
 
@@ -11,10 +14,50 @@ $config->banner = 'NEWS';
 
 get_header();
 
+//for each loop and date_diff function
+
+/*$datetime1 = new DateTime("10:00:00");
+$datetime2 = new DateTime(date("h:i:s"));
+//$interval = $datetime1->diff($datetime2);
+//$hours   = $interval->format('%h'); 
+//$minutes = $interval->format('%i');
+//echo 'Diff. in minutes is: '.($hours * 60 + $minutes); 
+$interval = date_diff($datetime1,$datetime2);
+$hours   = $interval->format('%h'); 
+$minutes = $interval->format('%i');
+$seconds = $interval->format('%s');
+$diffmin = ($hours * 60 + $minutes + $seconds/60);
+echo $diffmin;
+//echo $diff->format('%h:%i:%s'); */
+
 
 $myNewsList = new Newslist();
 
+
+foreach($myNewsList as $mnl)
+
+	{//get date 
+	$datetime[$mnl] = new DateTime(date("h:i:s"));
+	if ($mnl=1){$interval = new DateTime("0:0:0");} 
+	else {
+	$interval = date_diff($datetime[$mnl],$datetime[$mnl-1]);
+	}
+	$hours   = $interval->format('%h'); 
+	$minutes = $interval->format('%i');
+	$seconds = $interval->format('%s');
+	$diffmin = ($hours * 60 + $minutes + $seconds/60);
+	
+		if ($diffmin>10)
+		{//create an instance of constructor
+		echo $diffmin;
+		$myNewsList = new Newslist();
+	
+		} //else
+		
+	}//end for each loop
+	
 get_footer();
+
 
 class NewsList
 {
